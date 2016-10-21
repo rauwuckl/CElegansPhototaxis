c03e5e70747e6da26f12f9b46ed0bae7f9421223
rauwuckl <ch.23@web.de>
2016-10-21 10:50:13 +0100

    put in documentation

--- phototaxis.py
+++ phototaxis.py
@@ -6,7 +6,9 @@ from brian2 import *
 from my_helper import *
 from phototaxis_network import *
 import matplotlib.pyplot as plt
+import sys
 
+# uncomment for deterministic behaiviour:
 #set_noiseTerm('')
 
 
@@ -21,6 +23,7 @@ duration = endResponse - startResponse
 
 
 class Phototaxis_evaluator:
+# class that can evaluate the fitness of a parameterset 
 	def __init__(self, ID=None):
 		self.ID = ID
 		self.results = None
@@ -28,23 +31,16 @@ class Phototaxis_evaluator:
 		print('build finished')
 
 
-		# statemonSens = StateMonitor(network['sensory_neurons'], ['v','u'], record=True)
-		# synmon = StateMonitor(network['inter_connections'], ['g', 'Iinput_post'], record=True)
-		# statemon=StateMonitor(network['other_neurons'], ['v', 'u'], record=True)
 		other_spikeMon = SpikeMonitor(self.network['other_neurons'], name='spikemon_other')
 		self.network.add(other_spikeMon)
 
 		self.network.store()
 		print('storing complete')
-		###Make a graph
-		# DG = nx.DiGraph()
 
-		# DG.add_weighted_edges_from([(neuron_names['other'][edge[0]], neuron_names['other'][edge[1]], int(edge[2])) for edge in table])
-		# nx.write_graphml(DG, 'other.graphml')
 
-	### RUN
 	def assesPopulationFitness(self, pop, sender=None, numberAssesments=1):
 		# update all the fitness
+		# send results back threw the pipe
 		for individual in pop:
 			fit = self.assesNetworkFitnessNtimes(individual['content'], numberAssesments)
 			# Maybe put  a lock here
@@ -57,10 +53,9 @@ class Phototaxis_evaluator:
 		sender.send(pop)
 
 
-	def setNetworkFitness(self, individual):
-		individual['fitness']=self.assesNetworkFitness(individual['content'])
 
 	def assesNetworkFitness(self, parameter):
+		'''asseses network fitness for the given parameter'''
 		self.network.restore()
 		print('resoring complete')
 		set_parameter(self.network, parameter)
@@ -107,15 +102,23 @@ class Phototaxis_evaluator:
 #				axvline(spike/second, ls=':', c='y', lw=0.5)
 #			title('Other neuron: {}'.format(i))
 #		print(-penalty)
-	def assesBestVisual(self, name, N, j=0):
+
+
+	def assesBestVisual(self, name, N, saveFileAs=None, j=0):
+		'''call with .npy file name and number of runs. then the fittest individual from that file will be simulated and PLOTTED N times'''
 		pop= np.load(name)
 		best = pop[j]
 		for i in range(N):
 			ot,st,pen= self.simulateTraces(best)
-			plotResult(ot, st)
+			
+			if(not(saveFileAs is None)):
+				plotResult(ot, st, '{}{}.png'.format(saveFileAs,i))
+			else:
+				plotResult(ot,st)
 
 
 	def simulateTraces(self, individual):
+		'''simulate and save the traces for the motorneurons. so that they can be plotted'''
 		print('plotResult: asses Network visual:')
 		parameter=individual['content']
 		dispNet = buildNetwork()
@@ -142,7 +145,8 @@ class Phototaxis_evaluator:
 		print('network had fitness: {}'.format(tempFitnessList))
 		return np.mean(tempFitnessList)
 
-def plotResult(othermon, spiketrains):
+
+def plotResult(othermon, spiketrains, saveFileAs=None):
 	ion()
 	fig = plt.figure()
 	ax=fig.add_subplot(1,2,1)
@@ -179,27 +183,19 @@ def plotResult(othermon, spiketrains):
 
 	fig.subplots_adjust(bottom=0.3)
 	fig.legend([trace, startR, endR, spikesLine], ['membrane voltage', 'start of observed behaviour', 'end of observed behaviour', 'spike'], loc='lower center')
-	plt.show()
+	if saveFileAs is None:
+		plt.show()
+	else:
+		fig.savefig(saveFileAs)
 
 if __name__ == '__main__':
 	plt.close('all')
 	eva = Phototaxis_evaluator()
+	if len(sys.argv)!=1:
+		try:
+			N = int(sys.argv[2])
+			fileName = str(sys.argv[1])
+		except ValueError:
+			print('could not pars stuff: {}'.format(sys.argv))
 
-### PLOTTING
-
-# plotTracesFromStatemon(synmon, 'g', 'Iinput_post')
-# plotTracesFromStatemon(statemonSens,'v')
-# plotTracesFromStatemon(statemon, 'v')
-
-
-# def test():
-# 	start_scope()
-# 	neur=NeuronGroup(3, model_normal, threshold='v>30*mV', reset=reset)
-# 	s=Synapses(neur, neur, model=model_chem_syn, on_pre='g = 1')
-# 	s.connect(i=0,j=2)
-# 	s.w=1/ms
-# 	statemon=StateMonitor(neur, ['v','u'], record=True)
-
-# 	N = Network(neur,s)
-# 	N.run(500*ms)
-# 	plotTracesFromStatemon(statemon, 'v')
+		eva.assesBestVisual(fileName, N, 'bla')
